f3105b4694499627084aaca6910d57e24a8cd9bf
Arianne <amcanlubo@gmail.com>
2021-10-02 17:24:32 +0800

    added time and receiver and sender creds

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -1,8 +1,6 @@
 import React, { useContext, useEffect, useState, useRef } from 'react'
 import { Context } from './Store';
 import axios from 'axios'
-// import {UserGroupIcon} from '@heroicons/react/solid'
-import Modal from './Modal';
 import RightSideNav from './RightSideNav';
 
 const ChatForm = ({ userHeaders }) => {
@@ -13,14 +11,14 @@ const ChatForm = ({ userHeaders }) => {
     const [isLoading, setIsLoading] = useState(false)
     const [message, setMessage] = useState([])
 
-
+    let date,time
     const getMessage = () => {
         axios.get(`${url}/api/v1/messages?receiver_id=${state.ChatInfo.ID}&receiver_class=${state.ChatInfo.receiverClass}`, userHeaders)
             .then((response) => {
                 setMessage([])
                 if (response.data.errors) return null;
                 response.data.data.map((message) => setMessage((messages) => [...messages, message]))
-                console.log(response)
+                console.log(message)
                 
             })
             .catch((error) => {
@@ -31,20 +29,30 @@ const ChatForm = ({ userHeaders }) => {
             })
     }
 
-    
-
-    // //offset to maintain time zone difference
-    // const offset = yourDate.getTimezoneOffset();
-
-    // yourDate = new Date(yourDate.getTime() + (offset * 60 * 1000));
-    // let modifiedDate = yourDate.toISOString().split('T')[0]+" "+yourDate.toLocaleTimeString()
-
-    // console.log("Date you want --> ",modifiedDate)
-
     useEffect(() => {
         setIsLoading(true)
         getMessage()
+        convTime()
     }, [state])
+
+
+    const convTime = (time)=>{
+        let date = new Date(time)
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let amPm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // if hour '0' should be '12'
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        time = `${hours}:${minutes} ${amPm}`
+        // console.log(time)
+        return time
+
+    }
 
 
     const addMessage = (e) => {
@@ -65,19 +73,17 @@ const ChatForm = ({ userHeaders }) => {
             .catch((error) => alert(error))
     }
 
-
+    
     return (
-        <div className="w-100 relative flex-1 p:2 h-screen justify-between flex flex-col z-1">
+        <div className="w-100 relative flex-1 p:2 h-screen flex flex-col">
+            
 
-            <div class="absolute top-8 bg-secondary w-full text-white  flex items-center content-center justify-between px-5">
-                {/* //{state.ChannelInfo.channelName} */}
+            <div class="z-10 absolute top-8 bg-secondary w-full text-white  flex items-center content-center justify-between px-5">          
                 {state.ChatInfo.name}
-                {/* <Modal userHeaders={userHeaders} /> */}
                 <RightSideNav userHeaders={userHeaders} />
-
             </div>
 
-            <form class="absolute bottom-0 w-full bg-secondary z-0">
+            <form class="absolute bottom-0 w-full bg-secondary z-10">
                 <input
                     className="message-input w-5/6"
                     placeholder="Send a message..."
@@ -86,28 +92,28 @@ const ChatForm = ({ userHeaders }) => {
                 />
                 <button type="submit" className="send-button text-white font-semibold ml-10" onClick={addMessage} >SEND</button>
             </form>
-
-            <div className="flex sm:items-center justify-between border-b-2 border-gray-200 max-h-screen">
-                <div className='container flex flex-col justify-between sm:px-6'>
-
-                    <div class="py-3"></div>
-
+            
+            <div className="flex-1 sm:items-center px-5 py-12 overflow-auto h-36 mt-16  z-0 ">
+                
                     {!isLoading ?
-                    // flex flex-col py-12
-                        <div className='message-form-container overflow-auto h-screen pt-14'>
-                            
+                        <div> 
                                 {message.map((messages) => (
-                                   
-                                    <ul className={(messages.sender.uid === userHeaders.headers.uid)
+                                date = new Date(messages.created_at),
+                                time = convTime(messages.created_at),
+                                    <ul className='flex justify-items-start text-left'>
+                                        <div >
+                                        
+                                        <span className='px-1 text-sm'> {messages.sender.uid} </span>
+                                        <span className='px-1 text-sm'> {date.toString().slice(4,16)} </span>
+                                        <span className='px-1 text-sm'> {time} </span><br/>
+
+                                        <span className={(messages.sender.uid === userHeaders.headers.uid)
                                         ?
                                         'chat_bubble outgoing'
                                         :
                                         'chat_bubble incoming'
-                                    }>
-                                        <div className='block'>
-                                        {/* {messages.created_at} */}
-                                        {messages.sender.uid} <br/>
-                                        {messages.body}
+                                        }>
+                                        {messages.body}</span>
                                         </div>
                                     </ul>
                                 ))}
@@ -195,7 +201,7 @@ const ChatForm = ({ userHeaders }) => {
                     }
                 </div>
             </div>
-        </div>
+       
     )
 }
 
